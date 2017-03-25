04135399224e134ed9f69fbf5939cf82a13ce88a
Veer Gangwal <veer.gangwal@gmail.com>
2017-03-25 16:35:19 -0700

    committing literally all of my work, completely forgot to save commits

--- code/migrations/20160705125844_create_github_table.solution.js
+++ code/migrations/20160705125844_create_github_table.solution.js
@@ -1,12 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('repos', function (table) {
+  return knex.schema.createTable('repos', (table) => {
 
     table.string('name');
     table.string('owner');
-    table.primary(['name', 'owner']);
+    table.primary(['name', 'owner']); // both of these are primary
 
     table.integer('stargazers_count');
+    table.string('url');
   });
 };
 
--- code/server/index.js
+++ code/server/index.js
@@ -1,31 +1,57 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fetch = require('node-fetch');
 
-var app = express();
+const app = express();
 module.exports = app;
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 
-var knex = require('knex')({
+const knex = require('knex')({
   client: 'sqlite3',
   connection: {
-    filename: './github-fetcher.sqlite3'
-  }
+    filename: './github-fetcher.sqlite3',
+  },
+});
+
+app.post('/repos/import', (req, res) => {
+  const user = req.body.name;
+  fetch('https://api.github.com/users/' + user + '/repos')
+    .then((repoList) => {
+      return repoList.json();
+    }).then((json) => {
+      json.forEach((repoData) => {
+        knex('repos').insert([{
+          name: repoData.name,
+          owner: repoData.owner.login,
+          stargazers_count: repoData.stargazers_count,
+          url: repoData.html_url,
+        }])
+        .then(() => {
+          console.log('successfully added another repo!');
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+      });
+      res.sendStatus(201);
+    });
 });
 
 
-app.post('/repos/import', function (req, res) {
-  // TODO
+app.get('/repos', (req, res) => {
+  knex.select().table('repos').orderBy('stargazers_count', 'desc').limit(25)
+  .then((repos) => {
+    res.send(repos);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 });
 
-
-app.get('/repos', function (req, res) {
-  // TODO
-});
-
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/client/index.html')
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
 var port = process.env.PORT || 4040;
